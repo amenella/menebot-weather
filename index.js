361829f9b9cd24147b026f31618352ec7ed34f3b
Adrien Menella <adrien.menella@gmail.com>
2016-12-15 15:29:22 +0200

    add bot processing functions

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 
+// facebook page access token for the bot
+// https://www.facebook.com/Menebot-weather-214420989014876
+const token = process.env.FB_PAGE_ACCESS_TOKEN;
+
 app.set('port', (process.env.PORT || 5000));
 
 // Process application/x-www-form-urlencoded
@@ -30,3 +34,38 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function () {
   console.log('running on port', app.get('port'));
 });
+
+// Process received messages
+// TODO: intégrer wit
+app.post('/webhook/', function (req, res) {
+  let messagingEvents = req.body.entry[0].messaging;
+  for (let i = 0; i < messagingEvents.length; i++) {
+    let event = req.body.entry[0].messaging[i];
+    let sender = event.sender.id;
+    if (event.message && event.message.text) {
+      let text = event.message.text;
+      sendTextMessage(sender, 'Text received, echo: ' + text.substring(0, 200));
+    }
+  }
+  res.sendStatus(200);
+});
+
+// fucntion to send back response from bot
+function sendTextMessage (sender, text) {
+  let messageData = { text: text };
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: token},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: messageData
+    }
+  }, function (error, response, body) {
+    if (error) {
+      console.log('Error sending messages: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
